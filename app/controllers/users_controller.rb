77b3821eb282a17d92835aff35262184182ab76b
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-15 17:25:46 +0000

    setting routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+def index
+
+  render ({ :template => "user_templates/index.html.erb"})
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/users", { :controller=> "users", :action=> "index"})
+
+  
+  get("/photos", { :controller=> "photos", :action=> "index"})
+
 
 end
